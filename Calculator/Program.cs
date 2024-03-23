b88d7745233194595082c014fadc339b11ff9412
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2024-03-23 08:58:08 -0300

    example 1

---
+++ Calculator/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Sair");
+
+            Console.WriteLine("-------");
+            Console.WriteLine("Selecione uma opção: ");
+            short res = short.Parse(Console.ReadLine());
+
+            switch (res)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: System.Environment.Exit(0); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Soma()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            float result = v1 + v2;
+            Console.WriteLine("O resultado da soma é " + result);
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            float result = v1 - v2;
+            Console.WriteLine($"O resultado da subtração é {result}");
+            Console.ReadKey();
+            Menu();
+        }
+    }
+}
