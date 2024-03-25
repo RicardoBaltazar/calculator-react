f0958da49d58fe1ca849d6d781bb3b4b24a35b0c
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2024-03-25 14:56:19 -0300

    Example 2

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+
+            int multiplier = 0;
+
+            if (type == 's')
+                multiplier = 1;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            Start(time * multiplier);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(500);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(1000);
+        }
+    }
+}
